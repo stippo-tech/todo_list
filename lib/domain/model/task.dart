75ddc3f3b4c7eabd1a6d0010acdf575e43334487
Stepan Kovalev <65505037+stippo-tech@users.noreply.github.com>
2023-10-03 17:40:36 +0400

    feat: add task model

---
+++ lib/domain/model/task.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'task.freezed.dart';
+part 'task.g.dart';
+
+@freezed
+class Task with _$Task {
+  const Task._();
+
+  const factory Task({
+    required String id,
+    required String title,
+    required String url,
+    required bool completed,
+    String? order,
+  }) = _Task;
+
+  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+}
